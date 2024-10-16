dc490db778d72b560631bb0789a96e039dd9b71d
Ashvin Antyah <a.antyah@gmail.com>
2024-10-16 11:59:29 +0200

    start the project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ["index.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  /* npx tailwindcss build ./style/styles.css -o ./src/output.css --watch */
