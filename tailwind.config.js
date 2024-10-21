81cfc27462c5392f7a073db22c9b0e22a810e7c2
Ashvin Antyah <a.antyah@gmail.com>
2024-10-21 18:40:15 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ["index.html"],
     theme: {
-      extend: {},
+      extend: {
+        colors:{
+          "custom-purple":"rgb(118 81 220)",
+        }
+      },
     },
     plugins: [],
   }
